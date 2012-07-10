c4a677cde77e092ed66bb198cabd87feba034f40
Evana <evana@evana-iMac.(none)>
2012-07-10 12:16:58 -0400

    Deleted login authentication function

--- web/ws/appinventor.php
+++ web/ws/appinventor.php
@@ -1,30 +1,10 @@
 <?php
      require_once '../includes/config.php';
-     
-// Initalize authentication state to false, ie not logged in
-$authenticated = false;
+
 
 // Check for method parameter
 $method = (isset($_REQUEST['method']) ? safeString($_REQUEST['method']) : null);
 
-// Check for session key
-$session_key = (isset($_REQUEST['session_key']) ? safeString($_REQUEST['session_key']) : null);
-
-// Initalize Output Variables
-$status = 600;
-$data = null;
-
-// Check to see if session key is null, if not then log in
-if($session_key != null) {
-        $session->start_rest_session($session_key);
-} else {
-    
-    // Check to see if the request is to login, if so login
-    if(strcasecmp($method, "login") == 0) {
-        
-    }
-    
-}
 function getResults($method){      
           //The target for this test
           $target = "localhost/ws/api.php?method=". $method;
